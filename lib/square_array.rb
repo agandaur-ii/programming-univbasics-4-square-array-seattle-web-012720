de9780b9087cc3d01da74271c6341a26dbf37755
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2019-12-11 05:00:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,8 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do 
-    narray = []
-    narray.push([array[counter] ** 2])
+    puts array[counter] ** 2]
     counter += 1
-    return narray
   end
   return narray
 end
