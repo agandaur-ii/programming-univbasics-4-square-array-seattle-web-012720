e2a2a88a77c8069b33cdf42c5ff2a670f48ac904
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2019-12-11 04:56:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   narray = []
   
   while counter < array.length do 
-    puts narray.push(array[counter] ** 2)
+    narray.push(array[counter] ** 2)
     counter += 1
   end
   puts "I got here"
