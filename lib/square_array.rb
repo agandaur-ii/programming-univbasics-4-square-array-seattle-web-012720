1ab55ed1b37c0edd69ae18511749363b721eeb04
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2019-12-11 04:51:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 def square_array(array)
   counter = 0
-  new_array = [1]
+  narray = [1]
   
   while counter < array.length do 
-    new_array.push(array[counter] ** 2)
+    narray.push(array[counter] ** 2)
     counter += 1
   end
   puts "I got here"
-  return new_array
+  return narray
 end
 
 square_array([1, 2, 3])
